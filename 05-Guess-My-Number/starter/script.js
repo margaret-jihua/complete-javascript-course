502aaae15e3dc4f62a28c8a7a360be1b30aef853
margare-jihua <margaret.jihua@gmail.com>
2020-11-30 22:34:32 -0800

    Again btn and highscore functionality

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -5,7 +5,9 @@ let score = document.querySelector('.score')
 
 let answer = Math.floor(Math.random() * 20)
 let myScore = 20
+let highscore = 0
 
+// check btn 
 let check = document.querySelector('.check')
 check.addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value)
@@ -15,12 +17,17 @@ check.addEventListener('click', function() {
     }
     else if (guess === answer) {
         msg.textContent = '🎉 Correct Number!'
+        number.textContent = guess
         document.querySelector('body').style.backgroundColor = '#60b347'
         number.style.width = '30rem'
+        
+        if (myScore > highscore) {
+            highscore = myScore
+            document.querySelector('.highscore').textContent = highscore
+        }
     }
     else if (guess < answer) {
         msg.textContent = 'Too small!'
-        number.textContent = guess
         myScore --
         score.textContent = myScore
         if (myScore <= 0) {
@@ -29,11 +36,23 @@ check.addEventListener('click', function() {
     }
     else if (guess > answer) {
         msg.textContent = 'Too Big!'
-        number.textContent = guess
         myScore --
         score.textContent = myScore
         if (myScore <= 0) {
             msg.textContent = '💥 You lost the game!'
         }
     }
+})
+
+// again btn
+let again = document.querySelector('.again')
+again.addEventListener('click', function() {
+    answer = Math.floor(Math.random() * 20)
+    myScore = 20
+    msg.textContent = 'Start guessing...'
+    number.textContent = '?'
+    document.querySelector('.guess').value = ''
+    score.textContent = myScore
+    document.querySelector('body').style.backgroundColor = '#222'
+    number.style.width = '15rem'
 })
